151f9de08463ee94127409ce7d7d97d9daaba2b3
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-06-11 11:33:21 +0530

    project completed

--- car-price-api/streamlit_app.py
+++ car-price-api/streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Car Price Prediction", page_icon="🚗", layout="
 
 API_URL = (
     "https://fastapi-oi5y.onrender.com/predict"
-    or "http://127.0.0.1:8000/predict"
+   
 )  # change if your endpoint differs
 
 st.title("🚗 Car Price Prediction")
